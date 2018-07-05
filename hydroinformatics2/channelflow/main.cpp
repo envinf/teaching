856e4dd78e884e600c4e36212eead6a267e20afb
OlafKolditz <olaf.kolditz@ufz.de>
2018-07-05 21:04:43 +0200

    Update main.cpp

--- hydroinformatics2/channelflow/main.cpp
+++ hydroinformatics2/channelflow/main.cpp
@@ -1,1 +1,16 @@
-//main.cpp
+//Hydroinformatics-II
+//Channel flow
+//by Olaf Kolditz
+#include <QApplication>
+#include "dialog.h"
+
+int main(int argc, char *argv[])
+{
+  QApplication a(argc, argv);
+  Dialog w;
+  w.setWindowTitle("Newton Simulator");
+  //w.setFixedWidth(300);
+  w.setFixedWidth(450);
+  w.show();
+  return a.exec();
+}
